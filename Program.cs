d1d89cc0bed53e9ce5e82f0f167e49314950ae94
khorasanireyhan <khorasanireyhane23@gmail.com>
2025-10-09 14:13:13 +0330

    NumberToWord

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using c_sharp.reyhane_core.CodeTasks_Week1;
 using c_sharp.reyhane_core.If_Exercises._01_If_Statements;
 using c_sharp.reyhane_core.If_Exercises._03_NestedIf_Statements;
 using c_sharp.reyhane_core.If_Exercises._04_LogicalOperators_If;
+using c_sharp.reyhane_core.If_Exercises._05_Switch_Statements;
 using ReyhaneCore.IfExercises.NestedIfStatements;
 
 namespace c_sharp
@@ -30,22 +31,26 @@ namespace c_sharp
            
            /*04_logicalOperators_if:
                positiveNum.Num();
-               UserAge.AgeMetod();         */
-          
-           ProgramCondition.Condition();
-           //codeTasks_week1:
+               UserAge.AgeMethod();       
+               ProgramCondition.Condition();*/
+
+          /*05_Switch_Statements:
+           */
+         Programu.NumberToWord();
+
+          //codeTasks_week1:
           //CodeTasks.Maximum();
-           
-          
-          
-          
-          
-          
-           //Sahar_code
-           //DoWhileLoop.DoWhileLoopMethod();
-           //HealthCheck.CheckHealth();
-           //GuessGame.PlayGuessGame();
-           //MathOperations.PerformCalculations();
+
+
+
+
+
+
+          //Sahar_code
+          //DoWhileLoop.DoWhileLoopMethod();
+          //HealthCheck.CheckHealth();
+          //GuessGame.PlayGuessGame();
+          //MathOperations.PerformCalculations();
         }
     }
 }
---
+++ reyhane_core/If_Exercises/05_Switch_Statements/Programu.cs
@@ -0,0 +1,27 @@
+﻿namespace c_sharp.reyhane_core.If_Exercises._05_Switch_Statements;
+
+public class Programu
+{
+    //Write a program that takes a number between 1 and 3 and prints:
+    //one → 1, two → 2, or three → 3.
+
+    public static void NumberToWord()
+    { Console.WriteLine("Enter a number between 1 to 3");
+        int number = Convert.ToInt32(Console.ReadLine());
+        switch (number)
+        {
+            case 1: Console.WriteLine("one");
+                break;
+            case 2: Console.WriteLine("two");
+                break;
+            case 3: Console.WriteLine("three");
+                break;
+            
+            default:Console.WriteLine("the number is not between 1 and 3");
+                break;
+        }
+        
+
+
+    }
+}
